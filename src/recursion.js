2e917be882d7e35651ef39aa7bf36848f4c4e034
Minji Gwak <minjigwak@gmail.com>
2020-02-28 13:16:03 -0800

    updated countKeysInObj

--- src/recursion.js
+++ src/recursion.js
@@ -352,6 +352,21 @@ var rMap = function(array, callback) {
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
 var countKeysInObj = function(obj, key) {
+    var count = 0;
+
+    if (typeof obj === 'object') {
+        for (var prop in obj) {
+            if (prop === key) {
+                count++;
+            } 
+            if (typeof obj[prop] === 'object') {
+                count += countKeysInObj(obj[prop], key);
+            }
+        }
+    }
+
+    return count;
+
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
